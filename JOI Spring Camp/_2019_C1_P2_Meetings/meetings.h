5c3b9868f5b579eaabff6ec1c1cd00a5f8b44624
AQT <andrewqtgaming@gmail.com>
2020-11-13 23:38:11 -0500

    Removing all the not essential files

--- JOI Spring Camp/_2019_C1_P2_Meetings/meetings.h
+++
@@ -1,4 +0,0 @@
-void Solve(int N);
-
-int Query(int u, int v, int w);
-void Bridge(int u, int v);
--- JOI Spring Camp/_2019_C2_P3_TwoTransportation/Azer.h
+++
@@ -1,8 +0,0 @@
-#include <vector>
-
-void InitA(int N, int A, std::vector<int> U, std::vector<int> V,
-           std::vector<int> C);
-void ReceiveA(bool x);
-std::vector<int> Answer();
-
-void SendA(bool y);
